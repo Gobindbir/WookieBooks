54083f396f4fc09cce77f55a492712d54a971acf
Gobindbir <gobindnitj@gmail.com>
2021-02-06 08:39:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WookieBooks
+ 
---
+++ WookieBooks/Models/ReturnStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WookieBooks.Models
+{
+    public class ReturnStatus
+    {
+        public bool IsSuccess { get; set; }
+        public string ReturnMessage { get; set; }
+    }
+}
